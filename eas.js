a073736684f4a24fdbb71f7a656d730a723dada1
David <david-williams-1996@outlook.com>
2022-07-27 09:45:58 +1000

    Update variable names of columns and squares

--- eas.js
+++ eas.js
@@ -3,19 +3,20 @@
 function gridMaker (size) {
     //main container holding all other divs
     const container = document.querySelector('.container');
-    //create vertical divs in range of size variable
+    //create columns divs in range of size variable
     for (let i = 0; i < size; i++) {
-        const vertical = document.createElement('div');
-        vertical.classList.add(`block`);
-        container.appendChild(vertical);
-        //create horizontal divs in range of size var
+        const columns = document.createElement('div');
+        columns.classList.add('columns');
+        container.appendChild(columns);
+        //create squares divs in range of size var
         for (let j = 0; j < size; j++) {
-            const horizontal = document.createElement('div');
-            horizontal.classList.add('inline');
-            vertical.appendChild(horizontal);
+            const squares = document.createElement('div');
+            squares.classList.add('squares');
+            columns.appendChild(squares);
         }
 
     }
 }
 
-gridMaker(32);
+gridMaker(16);
+
